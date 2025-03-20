96db660eca16ff7fcbfd84371ab43afbd81ff1de
soumyasswt <somsah.official@gmail.com>
2025-03-21 00:17:17 +0530

    Fixed web-vitals import issue

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import dotenv from "dotenv";
 
 dotenv.config();
