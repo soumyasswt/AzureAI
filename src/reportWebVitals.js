d02d5f8cda04a3c9f1d50832d0c7b60de9f697b9
soumyasswt <somsah.official@gmail.com>
2025-03-21 00:14:42 +0530

    Fixed web-vitals import issue

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,7 +1,6 @@
-import { reportWebVitals } from 'web-vitals';
-reportWebVitals = onPerfEntry => {
+const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+    import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
